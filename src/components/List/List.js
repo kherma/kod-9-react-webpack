7f751d2354dd3ead46b24a3785c40f1cc7f583bd
kherma <kamil.herma01@gmail.com>
2025-12-04 19:19:45 +0100

    feat: add scss modules for Hero, List and SearchForm components

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,13 +1,17 @@
+import styles from './List.module.scss';
+
 const List = () => {
   return (
     <div>
-      <header>
-        <h2>
+      <header className={styles.header}>
+        <h2 className={styles.title}>
           Things to do <span>soon</span>
         </h2>
-        <p>Interesting things I want to check out</p>
       </header>
-      <section>
+      <p className={styles.description}>
+        Interesting things I want to check out
+      </p>
+      <section className={styles.columns}>
         <article>
           <h2>Books</h2>
         </article>
