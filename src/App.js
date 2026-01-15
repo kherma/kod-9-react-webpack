c4baed17f10797eac9419687538eef03c62fbda2
kherma <kamil.herma01@gmail.com>
2026-01-15 19:24:26 +0100

    feat: add react-router Routes for home, favorite and about pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Container from './components/Container/Container';
 import Home from './Pages/Home/Home';
+import Favorite from './Pages/Favorite/Favorite';
+import About from './Pages/About/About';
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Container>
-        <Home />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/favorite' element={<Favorite />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
       </Container>
     </main>
   );
--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,4 +1,4 @@
-import PageTitle from '../PageTitle/PageTitle';
+import PageTitle from '../../components/PageTitle/PageTitle';
 
 const About = () => (
   <div>
