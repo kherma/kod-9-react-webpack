779775b1e219ed481334337fac987328fe55e81f
kherma <kamil.herma01@gmail.com>
2026-01-15 18:53:10 +0100

    feat: add Favorite and About components

---
+++ src/components/About/About.js
@@ -0,0 +1,10 @@
+import styles from './About.module.scss';
+
+const About = () => (
+  <div>
+    <h1 className={styles.title}>About</h1>
+    <p>Lorem ipsum.</p>
+  </div>
+);
+
+export default About;
---
+++ src/components/Favorite/Favorite.js
@@ -0,0 +1,10 @@
+import styles from './Favorite.module.scss';
+
+const Favorite = () => (
+  <div>
+    <h1 className={styles.title}>Favorite</h1>
+    <p>Lorem ipsum.</p>
+  </div>
+);
+
+export default Favorite;
