4ce1ad848f5712141c6305026aafd89cebc01401
kherma <kamil.herma01@gmail.com>
2026-01-16 19:41:29 +0100

    feat: get title and description for List component from redux store

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,21 +2,21 @@ import styles from './List.module.scss';
 import Column from '../Column/Column';
 import ColumnForm from '../ColumnForm/ColumnForm';
 import { useSelector } from 'react-redux';
-import { getAllColumns } from '../../redux/store';
+import { getAllColumns, getListById } from '../../redux/store';
 
 const List = () => {
+  const LIST_ID = 1;
   const columns = useSelector(getAllColumns);
+  const { title, description } = useSelector(
+    (state) => getListById(state, LIST_ID) ?? {}
+  );
 
   return (
     <div>
       <header className={styles.header}>
-        <h2 className={styles.title}>
-          Things to do <span>soon</span>
-        </h2>
+        {title && <h2 className={styles.title}>{title}</h2>}
       </header>
-      <p className={styles.description}>
-        Interesting things I want to check out
-      </p>
+      {description && <p className={styles.description}>{description}</p>}
       <section className={styles.columns}>
         {columns.map((column) => (
           <Column key={column.id} {...column} />
